a0bd6cc7ddbb8d46de4551550965c9f36726349f
Philip Laine <philip.laine@gmail.com>
2016-10-11 14:58:56 +0200

    Add input validation

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ connection.connect();
 
 // App
 var express = require('express');
+var bodyParser = require("body-parser");
+var expressValidator = require('express-validator');
 var app = express();
+app.use(bodyParser.json());
+app.use(expressValidator([]));
 
 var server = app.listen(8080, function () {
   var host = server.address().address
@@ -20,61 +24,122 @@ var server = app.listen(8080, function () {
 })
 
 // Convenience
-function query(string, res) {
-  connection.query(string, function(err, rows, fields) {
+
+function returnQuery(query, res) {
+  connection.query(query, function(err, rows, fields) {
     if (err) {
-      console.log(err);
-      res.status(505).send('somethng went wrong');
-      return
+      res.status(500).send(err);
+      return;
     }
 
     res.send(rows);
   });
 }
 
-
 // User
 app.post('/user/create', function (req, res) {
-  var username = req.params['username'] || ''
-  var email = req.params['email'] || ''
-  query('INSERT INTO User (username, email) VALUES (\''+ username + '\', \'' + email +'\');', res)
+  req.check('username').notEmpty();
+  req.check('password').notEmpty();
+
+  if (req.validationErrors()) {
+    res.status(500).send();
+    return;
+  }
+
+  var sha1 = require('sha1');
+  var hahsedPassword = sha1(req.body.password);
+
+  var query = 'INSERT INTO User (username, password) VALUES (\''+ req.body.username + '\', \'' + hahsedPassword +'\');'
+  connection.query(query, function(err, rows, fields) {
+    if (err) {
+      var message = "";
+      if (err.code == 'ER_DUP_ENTRY') {
+        message = "not_unique_username";
+      }
+
+      res.status(500).send(message);
+      return;
+    }
+
+    returnQuery('SELECT * FROM User WHERE id = ' + rows.insertId + ';', res);
+  });
+})
+
+app.post('/user/authenticate', function (req, res) {
+  req.check('username').notEmpty();
+  req.check('password').notEmpty();
+
+  var sha1 = require('sha1');
+  var hashedPassword = sha1(req.body.password);
+
+  var query = 'SELECT * FROM User WHERE username = \'' + req.body.username + '\' AND password = \'' + hashedPassword + '\';'
+  connection.query(query, function(err, rows, fields) {
+    if (err) {
+      res.status(500).send();
+      return;
+    } else if (rows.length == 0) {
+      res.status(500).send('incorect_username_password');
+      return;
+    }
+
+    res.send(rows);
+  });
 })
 
 app.get('/user/:id(\\d+)/', function (req, res) {
-  query('SELECT * FROM User WHERE id = ' + req.params['id'] + ';', res)
+  returnQuery('SELECT * FROM User WHERE id = ' + req.params.id + ';', res)
 })
 
-/** Convenience really only used for testing and debug **/
-app.get('/user/list', function (req, res) {
-   query('SELECT * FROM User;', res)
-})
+/*app.post('/user/progress', function(req, res) {
+  var id = req.body.id;
+  var count = req.body.count;
+  var difficulty = req.body-parser.difficulty;
+  var score = 1;
 
+  var query = 'UPDATE User SET score = score + ' + score + ' WHERE id = ' +  id + ";";
+  connection.query(query, function(err, rows, fields) {
+    if (err) {
+      res.status(500).send();
+      return;
+    }
+
+    res.send();
+  });
+})*/
 
 // Game
 app.get('/game/:id(\\d+)/', function (req, res) {
-   query('SELECT * FROM Game WHERE id = ' + req.params['id'] + ';', res)
+  returnQuery('SELECT * FROM Game WHERE id = ' + req.params.id + ';', res)
 })
 
-/** Convenience really only used for testing and debug **/
-app.get('/game/list', function (req, res) {
-   query('SELECT * FROM Game;', res)
-})
-
+app.get('/game/current'), function (req, res) {
+  var query = 'SELECT * FROM Game WHERE player1 = ' + req.params.id + 'OR player2 = ' + req.params.id + ';';
+}
 
 // Image
 app.get('/game/:id(\\d+)/', function (req, res) {
-   query('SELECT * FROM Image WHERE id = ' + req.params['id'] + ';', res)
+   returnQuery('SELECT * FROM Image WHERE id = ' + req.params.id + ';', res)
 })
-
 
 // Question
-app.get('/question/:id(\\d+)/', function (req, res) {
-   query('SELECT * FROM Question WHERE id = ' + req.params['id'] + ';', res)
+app.get('/question/random', function (req, res) {
+  req.check('difficulty').notEmpty().isInt();
+  req.check('count').notEmpty().isInt();
+
+  if (req.validationErrors()) {
+    res.status(500).send();
+    return;
+  }
+
+  returnQuery('SELECT * FROM Question WHERE difficulty = ' + req.query.difficulty + ' ORDER BY RAND() LIMIT ' + req.query.count + ';', res)
 })
 
-app.get('/question/random', function (req, res) {
-  var difficulty = req.params['difficulty'] || 1;
-  var count = req.params['count'] || 4;
+app.get('/question/:id(\\d+)/', function (req, res) {
+   returnQuery('SELECT * FROM Question WHERE id = ' + req.params.id + ';', res)
+})
 
-  query('SELECT * FROM Question WHERE difficulty = ' + difficulty + ' ORDER BY RAND() LIMIT ' + count + ';', res)
+// Data
+
+app.get('/data/:id(\\d+)/', function (req, res) {
+   res.sendFile('/home/demo/data/images/' + req.params.id + '.png');
 })
